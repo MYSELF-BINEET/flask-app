82b97563e486c2cfa14e6e972458d46deafab4e5
BINEET PRADHAN <121740189+MYSELF-BINEET@users.noreply.github.com>
2026-01-21 00:52:29 +0530

    Remove unnecessary comments and whitespace

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# app.py  main file for application file
+# app.py 
 
 from flask import Flask 
 from urllib.parse import quote 
@@ -14,5 +14,6 @@ def create_app():
 if __name__ == '__main__':
     app = create_app()
     app.run(host='0.0.0.0', port=80, debug=True)
+
 
 
